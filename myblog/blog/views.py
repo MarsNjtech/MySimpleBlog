66a65e96e58ab750385b9b982565ab153d25556d
AustinDeng <13925510108@163.com>
2018-01-19 15:37:04 +0800

    Models

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 def index(requsest):
 #    return HttpResponse("Hello,World!")
-    return render(requsest, 'index.html', {'hello': 'Hello,World!!'})
+#    return render(requsest, 'index.html', {'hello': 'Hello,World!!'})
+    article = models.Article.objects.get(pk=1)
+    return render(requsest, 'index.html', {'article': article})
 # 第一个参数为对象本身，第二个为模板文件，字符串，第三个参数是后台传递到前端的数据
 # Create your views here.
