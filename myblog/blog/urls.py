b514b1714d710fdfaaf9e18686c56156ed389a25
AustinDeng <13925510108@163.com>
2018-01-21 23:22:21 +0800

    BasicFinished

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # path('index/',bv.index),
     path('index/', views.index),
     path('articles/<int:article_id>', views.article_page, name='article_page'),
-    path('edit/', views.edit_page, name='edit_page'),
+    path('edit/<int:article_id>', views.edit_page, name='edit_page'),
     path('edit/action', views.edit_action, name='edit_action')
 ]
