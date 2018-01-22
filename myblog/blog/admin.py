243f40bd5e0165622dedb2c51b34d9753111af28
AustinDeng <13925510108@163.com>
2018-01-22 14:39:11 +0800

    AdminFilter

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -3,4 +3,10 @@ from blog.models import Article
 
 
 # Register your models here.
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'pub_time')
+    list_filter = ('pub_time',)
+
+
+admin.site.register(Article, ArticleAdmin)
