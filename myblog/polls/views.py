291e745313d052ba59e49b2d69013f90266a47b2
AustinDeng <13925510108@163.com>
2018-01-29 20:14:37 +0800

    AddCss

--- myblog/polls/views.py
+++ myblog/polls/views.py
@@ -6,6 +6,7 @@ from .models import Question
 from .models import Choice
 from django.template import loader
 from django.http import Http404
+from django.views import generic
 
 
 # Create your views here.
@@ -35,9 +36,9 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
@@ -47,13 +48,33 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {'question': question,
-                                                     'erroe_message': "You did't select a choice."})
+                                                     'error_message': "You did't select a choice."})
     else:
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
+# 下面一个方法三个类均为通用视图
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ 返回最近时间的五个问题 """
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
